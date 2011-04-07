ca36661267c307bb34f050087436bd99a1ebffd7
syhw <gabriel.synnaeve@gmail.com>
2011-04-07 21:58:39 +0200

    vim with UTF-8 terminals support

---
+++ Library/Formula/vim.rb
@@ -0,0 +1,14 @@
+require 'formula'
+
+class Vim <Formula
+  url 'ftp://ftp.vim.org/pub/vim/unix/vim-7.3.tar.bz2'
+  homepage 'http://www.vim.org'
+  md5 '5b9510a17074e2b37d8bb38ae09edbf2'
+
+  def install
+    system "./configure", "--disable-debug", "--disable-dependency-tracking",
+                          "--enable-wide-chars", "--enable-multibyte",
+                          "--prefix=#{prefix}"
+    system "make install"
+  end
+end
