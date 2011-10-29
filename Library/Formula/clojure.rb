91cd040d0661fde7c75b342365ec8bb3752fe8b7
syhw <gabriel.synnaeve@gmail.com>
2011-10-29 18:17:19 +0200

    Upgrade to Clojure 1.3 release

--- Library/Formula/clojure.rb
+++ Library/Formula/clojure.rb
@@ -1,9 +1,10 @@
 require 'formula'
 
 class Clojure < Formula
-  url 'https://github.com/downloads/clojure/clojure/clojure-1.2.1.zip'
-  md5 'c5724c624fd6ce6a1d00252c27d53ebe'
-  head 'https://github.com/clojure/clojure.git'
+  url 'http://repo1.maven.org/maven2/org/clojure/clojure/1.3.0/clojure-1.3.0.zip'
+  md5 'de91ee9914017a38c7cc391ab8fcbc1a'
+  sha1 '7f5822ed847bb8a4c8ef93beed2d79248c20e1d1'
+  head 'https://github.com/clojure/clojure'
   homepage 'http://clojure.org/'
 
   def script; <<-EOS.undent
@@ -24,6 +25,8 @@ class Clojure < Formula
 
   def install
     system "ant" if ARGV.build_head?
+    system "mv clojure-#{version}.jar clojure.jar" # Because now clojure.jar is
+                                                   # named clojure-$VERSION.jar
     prefix.install 'clojure.jar'
     (prefix+'classes').mkpath
     (bin+'clj').write script
