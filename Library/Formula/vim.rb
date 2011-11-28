39169155935c4bef7fb989b3c8dc15a95fc5b5db
syhw <gabriel.synnaeve@gmail.com>
2011-11-28 14:27:56 +0100

    vim formula modifs

--- Library/Formula/vim.rb
+++ Library/Formula/vim.rb
@@ -6,9 +6,12 @@ class Vim <Formula
   md5 '5b9510a17074e2b37d8bb38ae09edbf2'
 
   def install
-    system "./configure", "--disable-debug", "--disable-dependency-tracking",
+    system "./configure", #"--disable-debug", "--disable-dependency-tracking",
                           "--enable-wide-chars", "--enable-multibyte",
-                          "--enable-pythoninterp",
+                          "--enable-pythoninterp", 
+                          "--with-python-config-dir=/usr/local/Cellar/python/2.7.2/Frameworks/Python.framework/Versions/2.7/lib/python2.7/config/",
+                          #"--with-macsdk='10.6'", "--with-features=huge",
+                          "--with-compiledby='syhw <[gabriel.synnaeve@gmail.com]>'",
                           "--prefix=#{prefix}"
     system "make install"
   end
